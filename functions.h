536fa65478d72ba86db9cbd17a1d62d6fc71a80c
JHeroGHJP <joaopedro.sanches@mail.com>
2018-08-16 15:55:51 -0300

    Entry beta

--- functions.h
+++ functions.h
@@ -3,4 +3,10 @@ struct no
     int numero;
     struct no *direita;
     struct no *esquerda;
-} 
+}
+
+struct nol
+{
+    int numero;
+    struct lista 
+}
--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <time.h>
 
 void main()
 {
     struct no *arvore;
     arvore = NULL;
+    
+    int numbers = rand()%INT_MAX;
+    
+    printf ("Quantidade de números: %i\n", numbers);
 }
